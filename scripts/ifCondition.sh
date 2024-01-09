c34b665656cf2a707a5ec51d85f2b05fd2e34999
iamkishorekumar-git <iamkishorekumar1@gmail.com>
2024-01-10 03:36:56 +0530

    Added ifcondition

--- scripts/ifCondition.sh
+++ scripts/ifCondition.sh
@@ -1,6 +1,6 @@
 #! bin/bash
 
-# VAR=10
+VAR=10
 NAME="test"
 #numberical Equal use => -eq
 #string Equal use > ==
@@ -11,4 +11,10 @@ elif [[ $NAME == "test" ]]; then
     echo "var contains test"
 else
     echo "no match"
+fi
+
+# another way for if condition check
+
+if test $NAME == "test1";then
+    echo "macthing through test fn"
 fi
