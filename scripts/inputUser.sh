5217c5d1614599df5f7156b3d8f5df7a3b04dd09
iamkishorekumar-git <iamkishorekumar1@gmail.com>
2024-01-11 06:45:03 +0530

    to check arguments

--- scripts/inputUser.sh
+++ scripts/inputUser.sh
@@ -6,5 +6,13 @@ echo "Your name is $name"
 
 #Arguments
 
+echo "Count is $#" # to get count of arguments
+
+if [[ $# -eq 0 ]]; then
+    echo "Please provide argument(s)."
+    exit 0
+fi
+
+
 echo "First argument: $1"
 echo "Second argument: $2"
